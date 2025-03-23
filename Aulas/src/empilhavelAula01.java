48dde1b226ef612edeb4832bf260c9fd052c21f6
Gabs-jg <gabriel19joao@gmail.com>
2025-03-23 18:47:31 -0300

    Códigos da aula 02

--- Aulas/src/empilhavelAula01.java
+++ Aulas/src/empilhavelAula01.java
@@ -1,47 +1,13 @@
-public interface empilhavelAula01 {
-
+public interface EmpilhavelAula01 {
     //Operações principais:
-    void empilhar(); //push - Adiciona um elemento no topo da pilha.
+    void empilhar(Object dado); //push - Adiciona um elemento no topo da pilha.
     Object desempilhar(); //pop - Remove e retorna o elemento que está no topo da pilha.
-    Object topo(); //top - Mostra o elemento que está no topo da pilhas mas não o remove.
+    Object espiar(); //peek - Mostra o elemento que está no topo da pilhas mas não o remove.
     void atualizar(Object dado); //update - Atualiza o valor do elemento do topo da pilha.
-
+    
     //Operações auxiliares:
     boolean estaCheia(); //isFull - overflow.
     boolean estaVazia(); //isEmpty - underflow.
     String imprimir(); //print
-
-    /*
-    --------------------------------
-    Conjunto de Estruturas de Dados:
-    --------------------------------
-    C - Create. (Criar);
-    R - Read. (Ler);
-    U - Update. (Atualizar);
-    D - Delete. (Deletar);
-    S - Sort. (Ordenar).
-    ---------------------------------
-
-    TIPOS DE DADOS:
-        - Numéricos(positivos ou negativos):
-            - Inteiros:
-                - unsigned int;
-                - tinyint, smallint;
-                - int;
-                - long, longint, bigint.
-            - Ponto flutuante:
-                - float;
-                - double.
-            - Ponto fixo.
-        - String = " ".
-        - Char = ' '.
-        - Lógico:
-            - boolean (bool).
-        - Matriz unidimensional:
-            - Vetor;
-            - Array.
-        - Ponteiro:
-            - É uma variável que guarda o endereço de um local de memória.
-        - Objeto.
-     */
 }
+    
