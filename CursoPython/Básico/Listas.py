43a6ccd980a458ceb783c2827adb4c0fe1a21f83
Gabs-jg <gabriel19joao@gmail.com>
2025-03-23 22:23:31 -0300

    Listas

---
+++ CursoPython/Básico/Listas.py
@@ -0,0 +1,11 @@
+#Exemplo 1:
+minha_lista = ["alfa", "beta", "teta", "gama", "sigma", "delta", "iota"] #Definindo lista
+print(minha_lista) #Mostra os elementos da lista.
+print(len(minha_lista)) #Mostra o tamanho da lista.
+
+#Exemplo 2:
+lista = [8, 9] #Definindo lista.
+lista.append(10) #Adiciona um novo elemento na lista.
+print(lista) #Mostra a lista atualizada.
+lista.append("palavra") #Adicionando uma string na lista.
+print(lista) #Mostra a lista atualizada.
