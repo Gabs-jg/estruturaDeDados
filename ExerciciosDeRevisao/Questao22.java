ec17834d3239d852934d135e3c93cf031ffc790e
gabs-jg <gabriel19joao@gmail.com>
2025-04-04 19:13:43 -0300

    Exercício 22

---
+++ ExerciciosDeRevisao/Questao22.java
@@ -0,0 +1,31 @@
+//22. Crie um algoritmo/fluxograma/programa que conte e imprima o número de vogais em um array de caracteres.
+
+package ExerciciosDeRevisao;
+import java.util.Scanner;
+
+public class Questao22 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Informe o tamanho da lista: ");
+        char lista[] = new char[sc.nextInt()];
+        char vogais[] = {'a', 'e', 'i', 'o', 'u'};
+        int quantVogais = 0;
+
+        for(int i=0; i<lista.length; i++) {
+            System.out.print("Digite o caracter da posição " + (i+1) + " da lista: ");
+            lista[i] = sc.next().charAt(0);
+        }
+
+        for(int i=0; i<lista.length; i++) {
+            for(int j=0; j<vogais.length; j++) {
+                if(lista[i]==vogais[j]) {
+                    quantVogais++;
+                }
+            }
+        }
+
+        System.out.println("A quantidade de vogais na lista é: " + quantVogais + ".");
+        sc.close();
+    }
+}
