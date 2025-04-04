25530406fd6b859ac7235b7f70906f531b9d1473
gabs-jg <gabriel19joao@gmail.com>
2025-04-04 14:48:08 -0300

    Exercício 19

---
+++ ExerciciosDeRevisao/Questao18.java
@@ -0,0 +1,60 @@
+//18. Crie um algoritmo/fluxograma/programa que multiplique os elementos de dois arrays e armazene o resultado em um terceiro array.
+
+package ExerciciosDeRevisao;
+import java.util.Scanner;
+
+public class Questao18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Informe o tamanho das listas que serão multiplicadas: ");
+        int lista1[] = new int[sc.nextInt()];
+        int lista2[] = new int[lista1.length];
+        int lista3[] = new int[lista1.length];
+
+        for(int i=0; i<lista1.length; i++) {
+            System.out.print("Digite o valor da posição " + (i+1) + " da lista 1: " );
+            lista1[i] = sc.nextInt();
+        }
+
+        for(int i=0; i<lista2.length; i++) {
+            System.out.print("D:igite o valor da posição " + (i+1) + " da lista 2: ");
+            lista2[i] = sc.nextInt();
+        }
+
+        for(int i=0; i<lista1.length; i++) {
+            lista3[i] = lista1[i] * lista2[i];
+        }
+
+        System.out.print("Lista 1: [");
+        for(int i=0; i<lista1.length; i++) {
+            if(i==lista1.length-1) {
+                System.out.print(lista1[i]);
+            } else {
+                System.out.print(lista1[i] + ", ");
+            }
+        }
+        System.out.println("]");
+
+        System.out.print("Lista 2: [");
+        for(int i=0; i<lista2.length; i++) {
+            if(i==lista2.length-1) {
+                System.out.print(lista2[i]);
+            } else {
+                System.out.print(lista2[i] + ", ");
+            }
+        }
+        System.out.println("]");
+
+        System.out.print("Lista resultado da multiplicação entre lista 1 e lista 2: [");
+        for(int i=0; i<lista3.length; i++) {
+            if(i==lista3.length-1) {
+                System.out.print(lista3[i]);
+            } else {
+                System.out.print(lista3[i] + ", ");
+            }
+        }
+        System.out.println("]");
+
+        sc.close();
+    }
+}
---
+++ ExerciciosDeRevisao/Questao19.java
@@ -0,0 +1,38 @@
+//19. Crie um algoritmo/fluxograma/programa que inicialize um array de strings e depois inverta a ordem dos elementos do array. Imprima o antes e o depois.
+
+package ExerciciosDeRevisao;
+import java.util.Scanner;
+
+public class Questao19 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Informe o tamanho da lista: ");
+        String lista[] = new String[sc.nextInt()];
+
+        for(int i=0; i<lista.length; i++) {
+            System.out.print("Digite a palavra da posição " (i+1) + " da lista: ");
+            lista[i] = sc.next();
+        }
+
+        System.out.print("Lista normal: [");
+        for(int i=0; i<lista.length; i++) {
+            if(i==lista.length-1) {
+                System.out.print(lista[i]);
+            } else {
+                System.out.print(lista[i + ", "]);
+            }
+        }
+        System.out.println("]");
+
+        System.out.print("Lista invertida: [")
+        for(int i=lista.length-1; i>=0; i--) {
+            if(i==0) {
+                System.out.print(lista[i]);
+            } else {
+                System.out.print(lista[i] + ", ");
+            }
+        }
+        System.out.println("]")
+
+    }
+}
