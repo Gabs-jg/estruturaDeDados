26be6f4b02088f4531e7e9d39b36c5df7353865e
gabs-jg <gabriel19joao@gmail.com>
2025-04-06 15:46:13 -0300

    Exercício 29

--- ExerciciosDeRevisao/Questao29.java
+++ ExerciciosDeRevisao/Questao29.java
@@ -1,22 +1,22 @@
-//28. Crie um algoritmo/fluxograma/programa que preencha um array com os primeiros N números da sequência de Fibonacci.
+//29. Crie um algoritmo/fluxograma/programa que preencha um array com os N primeiros termos da sequência de Lucas.
 
 package ExerciciosDeRevisao;
 import java.util.Scanner;
 
-public class Questao28 {
+public class Questao29 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Informe o tamanho da lista para obter a sequência Fibonacci: ");
+        System.out.print("Informe o tamanho da lista para obter a sequência de Lucas: ");
         int lista[] = new int[sc.nextInt()];
-        lista[0] = 0;
+        lista[0] = 2;
         lista[1] = 1;
 
         for(int i=2; i<lista.length; i++) {
-            lista[i] = lista[i - 1] + lista[i - 2];
+            lista[i] = lista[i -1] + lista[i - 2];
         }
 
-        System.out.print("Lista com os primeiros " + lista.length + " números da sequência de Fibonnaci: ");
+        System.out.print("Lista dos primeiros " + lista.length + " números da sequência de Lucas: ");
         for(int i=0; i<lista.length; i++) {
             if(i==lista.length-1) {
                 System.out.print(lista[i]);
