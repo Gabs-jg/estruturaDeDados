8155ddf0daa0c87a6f92d74f8246750bcb6d4a88
Gabs-jg <gabriel19joao@gmail.com>
2025-04-07 03:47:39 -0300

    Exercício 35

--- ExerciciosDeRevisao/Questao35.java
+++ ExerciciosDeRevisao/Questao35.java
@@ -1,13 +1,12 @@
-//34. Crie um algoritmo/fluxograma/programa que insira um elemento em uma posição específica de um array.
+//35. Crie um algoritmo/fluxograma/programa que remova um elemento de uma posição específica de um array. Diminuindo o tamanho do array.
 
 package ExerciciosDeRevisao;
 import java.util.Scanner;
 
-public class Questao34 {
+public class Questao35 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        System.out.print("Informe o tamanho da lista: ");
+        System.out.print("Digite o tamanho da lista: ");
         int lista[] = new int[sc.nextInt()];
 
         for(int i=0; i<lista.length; i++) {
@@ -25,31 +24,26 @@ public class Questao34 {
         }
         System.out.println("]");
 
-        System.out.print("Digite o número que queira adicionar na lista: ");
-        int num = sc.nextInt();
-
-        System.out.print("Informe a posição que queira adicionar o número: ");
+        System.out.print("Informe a posição da lista em que deseja remover o elemento: ");
         int posicao = sc.nextInt();
 
-
-        if(posicao<0 || posicao>lista.length) {
+        if(posicao < 0 || posicao>lista.length) {
             System.out.println("Posição inválida.");
             sc.close();
             return;
         }
 
-        int novaLista[] = new int[lista.length+1];
+        int novaLista[] = new int[lista.length-1];
+
         for(int i=0; i<posicao; i++) {
             novaLista[i] = lista[i];
         }
 
-        novaLista[posicao] = num;
-
-        for(int i=posicao; i<lista.length; i++) {
-            novaLista[i+1] = lista[i];
+        for(int i=posicao+1; i<lista.length; i++) {
+            novaLista[i - 1] = lista[i];
         }
 
-        System.out.print("Lista com elemento adicionado: [");
+        System.out.print("Lista com a exclusão do elemento da posição " + posicao + ": [");
         for(int i=0; i<novaLista.length; i++) {
             if(i==novaLista.length-1) {
                 System.out.print(novaLista[i]);
