c592a6c41e6204f03ff4159aa188164a4f08586e
gabs-jg <gabriel19joao@gmail.com>
2025-04-06 21:48:44 -0300

    Exercício 31 parcial

---
+++ ExerciciosDeRevisao/Questao31.java
@@ -0,0 +1,43 @@
+//31. Crie um algoritmo/fluxograma/programa que rotacione os elementos de um array para a esquerda.
+
+package ExerciciosDeRevisao;
+import java.util.Scanner;
+
+public class Questao31 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Informe o tamanho da lista: ");
+        int lista[] = new int[sc.nextInt()];
+
+        for(int i=0; i<lista.length; i++) {
+            System.out.print("Digite o valor da posição " + (i+1) + " da lista: ");
+            lista[i] = sc.nextInt();
+        }
+
+        System.out.print("Lista original: [");
+        for(int i=0; i<lista.length; i++) {
+            if(i==lista.length-1) {
+                System.out.print(lista[i]);
+            } else {
+                System.out.print(lista[i] + ", ");
+            }
+        }
+        System.out.println("]");
+
+        for(int i=0; i<lista.length; i++) {
+            lista[i] = lista[(lista.length-1) - i];
+        }
+
+        System.out.print("Lista rotacionada para a esquerda: [");
+        for(int i=0; i<lista.length; i++) {
+            if(i==lista.length-1) {
+                System.out.print(lista[i]);
+            } else {
+                System.out.print(lista[i] + ", ");
+            }
+        }
+        System.out.println("]");
+        sc.close();
+    }
+}
