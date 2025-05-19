281f98a02fbe88d6a321ea5937e2002b71f326c1
gabs-jg <gabriel19joao@gmail.com>
2025-05-19 19:45:21 -0300

    Códigos da primeira aula da segunda unidade - Pilha dinâmica

---
+++ ExerciciosDeRevisao/Lista1/Questao6.java
@@ -0,0 +1,11 @@
+//6. Faça um programa que peça as quatro notas de 10 alunos, calcule e armazene num vetor a média de cada aluno, imprima o número de alunos com média maior ou igual a 7.0.
+
+package ExerciciosDeRevisao.Lista1;
+
+import java.util.Scanner;
+
+public class Questao6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
