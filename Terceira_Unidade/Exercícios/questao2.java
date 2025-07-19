a0e7644d9d612eea4303920a1394df5bc3ce60c2
Gabs-jg <gabriel19joao@gmail.com>
2025-07-19 20:14:14 -0300

    Método main

--- Terceira_Unidade/Exercícios/questao2.java
+++ Terceira_Unidade/Exercícios/questao2.java
@@ -11,6 +11,18 @@ public class questao2 {
         // Soma folhas da esquerda e direita
         return contarFolhas(raiz.esquerda) + contarFolhas(raiz.direita);
     }
+
+    public static void main(String[] args) {
+        No raiz = new No(1);
+        raiz.esquerda = new No(2);
+        raiz.direita = new No(3);
+        raiz.esquerda.esquerda = new No(4);
+        raiz.esquerda.direita = new No(5);
+        raiz.direita.esquerda = new No(6);
+
+        int totalFolhas = contarFolhas(raiz);
+        System.out.println("Total de folhas: " + totalFolhas); // Saída esperada: 3 (nós 4, 5, 6)
+    }
 }
 
 // Reutilizando a classe No do exercício anterior
