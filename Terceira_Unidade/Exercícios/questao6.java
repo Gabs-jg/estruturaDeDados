54864d2c327fdb22941e6f4e44fc06db40da24f0
Gabs-jg <gabriel19joao@gmail.com>
2025-07-20 18:13:13 -0300

    no

---
+++ Terceira_Unidade/Exercícios/questao6.java
@@ -0,0 +1,15 @@
+package Terceira_Unidade.Exercícios;
+
+public class questao6 {
+    
+}
+
+class No {
+    int valor;
+    No esquerda, direita;
+
+    public No(int valor) {
+        this.valor = valor;
+        esquerda = direita = null;
+    }
+}
