dfdc5cf73285e23ca64c9e052de7b15871200eba
Gabs-jg <gabriel19joao@gmail.com>
2025-07-20 17:00:11 -0300

    Método calcularAltura

--- Terceira_Unidade/Exercícios/questao3.java
+++ Terceira_Unidade/Exercícios/questao3.java
@@ -1,7 +1,16 @@
 package Terceira_Unidade.Exercícios;
 
 public class questao3 {
-    
+    // Método recursivo para calcular altura da árvore
+    public static int calcularAltura(No raiz) {
+        if (raiz == null) {
+            return 0; // Árvore vazia tem altura 0
+        }
+        int alturaEsquerda = calcularAltura(raiz.esquerda);
+        int alturaDireita = calcularAltura(raiz.direita);
+
+        return 1 + Math.max(alturaEsquerda, alturaDireita);
+    }
 }
 
 class No {
