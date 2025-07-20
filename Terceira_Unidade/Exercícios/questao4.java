265bf1ef4aa7039af6be9fbaca1ef98d9d2b31e5
Gabs-jg <gabriel19joao@gmail.com>
2025-07-20 17:28:03 -0300

    Método main

--- Terceira_Unidade/Exercícios/questao4.java
+++ Terceira_Unidade/Exercícios/questao4.java
@@ -11,6 +11,18 @@ public class questao4 {
         }
         return contarNosFolha(raiz.esquerda) + contarNosFolha(raiz.direita);
     }
+
+    public static void main(String[] args) {
+        No raiz = new No(1);
+        raiz.esquerda = new No(2);
+        raiz.direita = new No(3);
+        raiz.esquerda.esquerda = new No(4);
+        raiz.esquerda.direita = new No(5);
+        raiz.direita.direita = new No(6);
+
+        int folhas = contarNosFolha(raiz);
+        System.out.println("Total de nós folha: " + folhas); // Saída esperada: 3 (nós 4, 5, 6)
+    }
 }
 
 class No {
