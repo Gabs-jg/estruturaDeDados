e139fabdd57ea4f0fff517e8a50aacf715aeb0fb
Gabs-jg <gabriel19joao@gmail.com>
2025-07-20 18:15:19 -0300

    Método encontrarMaiorValor

--- Terceira_Unidade/Exercícios/questao6.java
+++ Terceira_Unidade/Exercícios/questao6.java
@@ -1,7 +1,17 @@
 package Terceira_Unidade.Exercícios;
 
 public class questao6 {
-    
+     // Retorna o maior valor encontrado na árvore
+    public static int encontrarMaiorValor(No raiz) {
+        if (raiz == null) {
+            return Integer.MIN_VALUE; // Valor mínimo possível para comparação
+        }
+
+        int maiorEsquerda = encontrarMaiorValor(raiz.esquerda);
+        int maiorDireita = encontrarMaiorValor(raiz.direita);
+
+        return Math.max(raiz.valor, Math.max(maiorEsquerda, maiorDireita));
+    }
 }
 
 class No {
